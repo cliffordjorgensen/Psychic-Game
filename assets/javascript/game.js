b1f85fee55ef28b6169310d173f03c7acbb65709
cLifFORD JoRGenSen <cliffordjorgensen@gmail.com>
2019-05-23 19:05:03 -0700

    Psych

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,42 @@
-
-var chances = 4;
-var win = 0;
-var alpha = [];
-for (var i = 65; i <= 90; i++) {
-    res = String.fromCharCode(i);
-    alpha.push(res);
-}
-console.log(alpha);
-
+///Cliff Jorgensen///
 randNum = () => {
-    var genRand = Math.floor(Math.random() * 90) + 65
-    return genRand
+    var genRand = Math.floor(Math.random() * 97) + 23
+    return genRand;
+}
+var wins = 0;
+var losses = 0;
+var geussCount = 0;
+var computerChoices = String.fromCharCode(randNum());
+var directionsText = document.getElementById("directions-text");
+var userChoiceText = document.getElementById("userchoice-text");
+var computerChoiceText = document.getElementById("computerchoice-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var geussCountText = document.getElementById("geussCount-text");
+document.onkeyup = function (event) {
+    var userGuess = event.key;
+    for (i = 0; i < 10; i++) {
+        var computerChoices = String.fromCharCode(randNum())
+    }
+    if (userGuess === computerChoices) {
+        wins++;
+        alert("YOuWinnnnN!!")
+    } else {
+        geussCount++;
+        losses++;
+    }
+    if (geussCount > 10) {
+        alert("YOuLosE!!");
+        alert("press OK to play again")
+        wins = 0;
+        losses = 0;
+        geussCount = 0;
+    };
+    userChoiceText.textContent = "You chose: " + userGuess;
+    computerChoiceText.textContent = "The computer chose: " + computerChoices;
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "osses: " + losses;
+    geussCountText.textContent = "Geuss Count: " + geussCount;
+
 
 }
-
-var compRand = String.fromCharCode(randNum())
-console.log(compRand)
-
-
-
-
-
-
-
-
-
